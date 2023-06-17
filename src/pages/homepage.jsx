b2c6ed7f4bd5e057d4c8c407782d0184ae4a5a79
AKASH P S <akashchakkalaveli223@gmail.com>
2023-06-17 19:50:03 +0530

    updated

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -5,26 +5,26 @@ import { Link } from "react-router-dom";
 export function Home() {
   return (
     <div className="home_page">
-      <div className="rectangle01"></div>
-      <div className="projecx_flow">ProjecX flow</div>
-      <div className="rectangle02"></div>
-      {<Link to="/pages/loginpage.jsx">Login</Link>}
-      <p className="title">Manage projects with ProjectXFlow</p>
+      <p className="title">ProjecXFlow</p>
+      <div className="login_rect"></div>
+      <div className="login_key">Login</div>
+      <div className="search_rect"></div>
+      <input
+        className="search"
+        defaultValue="Search Project"
+        onClick={(event) => (event.target.value = "")}
+      />
+      <div className="mini_rect"></div>
+      <p className="miniproject">Mini Project</p>
+      <div className="main_rect"></div>
+      <p className="miniproject">Main Project</p>
+      <p className="tagline_head">Manage projects with ProjectXFlow</p>
       <p className="tagline">
         All in one solution for project management. Here you can simply manage
         your projects under the guidance of your faculties.
       </p>
-      <div className="rectangle03"></div>
-      <p className="search">Search Project</p>
-      <div className="item">|</div>
-      <img className="glass_img" src="./assets/magnifying-glass-1-38a.png" />
-      <img className="projectxflow_logo" />
-      <div className="rectangle04"></div>
-      <div className="main_proj">Main Project</div>
-      <div className="rectangle05"></div>
-      <div className="mini_proj">Mini Project</div>
-      <div className="rectangle06"></div>
-      <p className="start">Get started</p>
+      <div className="signup_rect"></div>
+      <p className="getstarted">Get Started</p>
     </div>
   );
 }
