527c74742f7bec9522aaf36ce4abb20e38b5343b
AKASH P S <akashchakkalaveli223@gmail.com>
2023-06-18 12:13:41 +0530

    done

--- src/pages/forgotpassword.jsx
+++ src/pages/forgotpassword.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import "../styles/forgotpassword.css";
+import { Link } from "react-router-dom";
 
 function ForgotPassword() {
   return (
-    <div>
-      <div className="forgot">
-        <p className="title">Let's Recover Your Account</p>
-        <div className="bgrect"></div>
-        <input className="email"></input>
-        <p className="qn">What is your favorite Place ?</p>
-        <div className="ans"></div>
-      </div>
+    <div className="forgot">
+      <p className="title">Let's Recover Your Account</p>
+      <div className="bgrect"></div>
+      <input
+        className="email"
+        defaultValue={"Enter Your Email ID"}
+        onClick={(event) => (event.target.value = "")}
+      ></input>
+      <p className="question">What is your favorite Place ?</p>
+      <input
+        className="answer"
+        defaultValue={"Enter Your Answer"}
+        onClick={(event) => (event.target.value = "")}
+      ></input>
+      <Link to="/src/pages/loginpage.jsx">
+        <div className="submit">Submit</div>
+      </Link>
     </div>
   );
 }
