0d47b08432634ef68efb20e4043d17bce27058ec
MJitsme <milanjoseph714@gmail.com>
2023-06-19 13:03:55 +0530

    update

---
+++ server/models/cordinatormodel.js
@@ -0,0 +1,43 @@
+const mongoose=require ('mongoose')
+
+const cordinatorSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:[true,'Email should be present']
+    },
+    name:{
+        type:String,
+        required:[true,'Username should be present'],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true,'password field should be present']
+    },
+    phoneno:{
+        type:Number,
+        required:[true,'phone number field should be present']
+    },
+    college:{
+        type:String,
+        required:[true,'college field should be present']
+    },
+    dept:{
+        type:String,
+        required:[true,'department field should be present']
+    },
+    question:{
+        type:String,
+        required:[true,'question field should be present']
+    },
+    answer:{
+        type:String,
+        required:[true,'answer field should be present']
+    }
+
+
+},{timestamps:true});
+
+const Cordinator = mongoose.model("cordinator", cordinatorSchema);
+
+module.exports = Cordinator;
