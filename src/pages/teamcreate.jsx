1a1fa61435fb4b47119e2d8b0289b8b791fd2c56
AKASH P S <akashchakkalaveli223@gmail.com>
2023-06-23 11:18:20 +0530

    fix: form bugs at team create

--- src/pages/teamcreate.jsx
+++ src/pages/teamcreate.jsx
@@ -1,65 +1,55 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../styles/teamcreate.css";
 
 function TeamCreate() {
+  const yearRef = useRef(null);
+  const handleSubmit = () => {
+    console.log("ge");
+    alert("AFFs");
+    // alert(yearRef.current.value);
+    let year = yearRef.current.value;
+  };
+  // console.log("hey");
   return (
     <div className="team">
-      <form>
+      <form
+        onSubmit={() => {
+          console.log("herer");
+        }}
+      >
         <div className="dash">Creating a Dashboard</div>
-        <input
-          className="proname"
-          defaultValue="Project Title"
-          onClick={(event) => (event.target.value = "")}
-        />
-        <input
-          className="protype"
-          defaultValue="Project Type"
-          onClick={(event) => (event.target.value = "")}
-        />
-        <input
+        <input className="proname" placeholder="Project Title" />
+        <input className="protype" placeholder="Project Type" />
+        <select className="year" ref={yearRef}>
+          <option>a</option>
+          <option>b</option>
+          <option>c</option>
+          <option>d</option>
+          <option>e</option>
+        </select>
+        {/* <input
           className="year"
-          defaultValue="Year"
+          placeholder="Year"
           onClick={(event) => (event.target.value = "")}
-        />
+        /> */}
         <div className="teamadd">Add Your Team Members</div>
-        <input
-          className="teammate_one"
-          defaultValue="Teammate's Email ID"
-          onClick={(event) => (event.target.value = "")}
-        />
-        <input
-          className="teammate_two"
-          defaultValue="Teammate's Email ID"
-          onClick={(event) => (event.target.value = "")}
-        />
-        <input
-          className="teammate_three"
-          defaultValue="Teammate's Email ID"
-          onClick={(event) => (event.target.value = "")}
-        />
-        <input
-          className="teammate_four"
-          defaultValue="Teammate's Email ID"
-          onClick={(event) => (event.target.value = "")}
-        />
+        <input className="teammate_one" placeholder="Teammate's Email ID" />
+        <input className="teammate_two" placeholder="Teammate's Email ID" />
+        <input className="teammate_three" placeholder="Teammate's Email ID" />
+        <input className="teammate_four" placeholder="Teammate's Email ID" />
         <div className="facultyadd">Add Faculties To Dashboard</div>
-        <input
-          className="coordinator01"
-          defaultValue="Coordinator's Email ID"
-          onClick={(event) => (event.target.value = "")}
-        />
-        <input
-          className="coordinator02"
-          defaultValue="Coordinator's Email ID"
-          onClick={(event) => (event.target.value = "")}
-        />
-        <input
-          className="guide"
-          defaultValue="Guide's Email ID"
-          onClick={(event) => (event.target.value = "")}
-        />
+        <input className="coordinator01" placeholder="Coordinator's Email ID" />
+        <input className="coordinator02" placeholder="Coordinator's Email ID" />
+        <input className="guide" placeholder="Guide's Email ID" />
         <div className="bgrect"></div>
-        <button className="submit">Submit</button>
+        <input
+          className="submit"
+          type="submit"
+          value="SUbmit"
+          onClick={() => {
+            console.log("sdfsfd");
+          }}
+        />
       </form>
     </div>
   );
